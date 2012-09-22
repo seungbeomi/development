ffe5fed75bdf7d6fb4dc10de2b4883f2231af378
seungbeomi <seungbeomi@gmail.com>
2012-09-22 16:39:24 +0900

    java hibernate tutorial part10

--- java/hibernate/spring-hibernate/src/main/java/com/hibernate/chapter01/Employee.java
+++ java/hibernate/spring-hibernate/src/main/java/com/hibernate/chapter01/Employee.java
@@ -1,17 +1,45 @@
 package com.hibernate.chapter01;
 
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.TableGenerator;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 
 import lombok.Data;
 
 @Data
 @Entity
+@Table(name="EmployeeInfo")
 public class Employee {
 
 	@Id
+	@TableGenerator(name="empid", 
+		table="emppktb", pkColumnName="empkey", pkColumnValue="empvalue",
+		allocationSize=1)
+	@GeneratedValue(strategy=GenerationType.TABLE, generator="empid")
+	@Column(name="EmployeeId")
 	private int empId;
 	private String empName;
+	@Transient
+	private String empPassword;
+	@Column(nullable=false)
+	private String empEmailAddress;
+	@Basic
+	private boolean isPermanent;
+	@Temporal(TemporalType.DATE)
+	private Calendar empJoinDate;
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date empLoginTime;
 	
 	public Employee() {}
 	public Employee(int empId, String empName) {
