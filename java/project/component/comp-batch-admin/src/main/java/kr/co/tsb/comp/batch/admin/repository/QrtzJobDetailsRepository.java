d2f24d4fefd0286f9edad385cafdf2441236e49c
seungbeomi <seungbeomi@gmail.com>
2012-10-23 20:40:23 +0900

    create first hibernate repository

---
+++ java/project/component/comp-batch-admin/src/main/java/kr/co/tsb/comp/batch/admin/repository/QrtzJobDetailsRepository.java
@@ -0,0 +1,23 @@
+/**
+ * QrtzJobDetailsRepository.java
+ *
+ * Created on   : 2012. 10. 23.
+ * Target OS    : Java VM 1.6.0.25 
+ * CVS revision : $Revision:  
+ *
+ * ------------------------------------------------------------
+ * CHANGE REVISION
+ * ------------------------------------------------------------
+ * DATE				AUTHOR		REVISION    
+ * 2012. 10. 23.		seungbeomi		First release.
+ */
+package kr.co.tsb.comp.batch.admin.repository;
+
+/**
+ * 클래스의 내용을 입력해주세요.
+ * 
+ * @author seungbeomi
+ */
+public interface QrtzJobDetailsRepository {
+
+}
