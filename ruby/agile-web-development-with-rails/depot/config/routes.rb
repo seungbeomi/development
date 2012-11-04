b24bd68a3a7dedc7021f4222b055b0a73b35a5f5
seungbeomi <seungbeomi@gmail.com>
2012-11-04 13:15:49 +0900

    depot정리

--- ruby/agile-web-development-with-rails/depot/config/routes.rb
+++ ruby/agile-web-development-with-rails/depot/config/routes.rb
@@ -57,7 +57,7 @@ Depot::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-  root :to => 'store#index', as: 'store' 
+  root :to => 'store#index', as: 'store' # store_path 변수생성
  
   # See how all your routes lay out with "rake routes"
 
